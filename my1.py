5a60ab7725e66ad26c334e2be5c0ba8427de077a
AlexeyRachuk <alexeyrachuk94@gmail.com>
2021-11-30 00:07:07 +0700

    com2

--- my1.py
+++ my1.py
@@ -1,9 +1,30 @@
+import os
 import telebot
-from telebot import types
+from flask import Flask, request
 
-# 2122815268:AAHXEstUmm_bFxw8yiw0HHOYjnn4MdvZ2ek
+token = "2122815268:AAHXEstUmm_bFxw8yiw0HHOYjnn4MdvZ2ek"
+app_url = f"https://raccoonmehbot.herokuapp.com/{token}"
 
 bot = telebot.TeleBot("2122815268:AAHXEstUmm_bFxw8yiw0HHOYjnn4MdvZ2ek")
+server = Flask(__name__)
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
 
 name = ''
 age = 0
@@ -16,7 +37,7 @@ def command_help(message):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    # bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEDYZBhpMKiuYTgWQSnfxq8gJVsUf3c2gAC3AsAAmbEWEtneEZg3zITKCIE")
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEDYZBhpMKiuYTgWQSnfxq8gJVsUf3c2gAC3AsAAmbEWEtneEZg3zITKCIE")
 
     if message.text == 'хех':
         bot.reply_to(message, 'хых')
