05f94014cbdce29ef77d6f0b6a94a8e47f2f6264
AlexeyRachuk <alexeyrachuk94@gmail.com>
2021-12-02 11:17:48 +0700

    com8

--- my1.py
+++ my1.py
@@ -37,65 +37,9 @@ def command_help(message):
     bot.reply_to(message, "Привмяу, че кого?")
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEDYZBhpMKiuYTgWQSnfxq8gJVsUf3c2gAC3AsAAmbEWEtneEZg3zITKCIE")
-
-    if message.text == 'хех':
-        bot.reply_to(message, 'хых')
-    elif message.text == '/reg':
-        bot.send_message(message.from_user.id, 'Ты кто?')
-        bot.register_next_step_handler(message, reg_name)
-
-
-def reg_name(message):
-    global name
-    name = message.text
-    bot.send_message(message.from_user.id, 'Скока лет?')
-    bot.register_next_step_handler(message, reg_age)
-
-
-def reg_age(message):
-    global age
-    while age == 0:
-        try:
-            age = int(message.text)
-        except Exception:
-            bot.send_message(message.from_user.id, "Ой, что-то не так... тебе нужно вводить возраст цифрами.")
-
-    keyboard = types.InlineKeyboardMarkup()
-    key_yes = types.InlineKeyboardButton(text="ды", callback_data="yes")
-    keyboard.add(key_yes)
-    key_no = types.InlineKeyboardButton(text="ноуп", callback_data="no")
-    keyboard.add(key_no)
-    question = 'Тебе ' + str(age) + " годиков. И ты " + name + "?"
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, "норм, сохраню")
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id, "а чё?")
-        bot.send_message(call.message.chat.id, 'Ты кто?')
-        bot.register_next_step_handler(call.message, reg_name)
-
-
-
-from telebot import types
-
-@bot.message_handler(commands=['button'])
-def start(message):
-  markup = types.InlineKeyboardMarkup()
-  buttonA = types.InlineKeyboardButton('A', callback_data='a')
-  buttonB = types.InlineKeyboardButton('B', callback_data='b')
-  buttonC = types.InlineKeyboardButton('C', callback_data='c')
-
-  markup.row(buttonA, buttonB)
-  markup.row(buttonC)
-
-  bot.send_message(message.chat.id, 'It works!', reply_markup=markup)
-
+@bot.message_handler(commands=['Когда', 'когда', 'Туса', 'туса'])
+def command_help(message):
+    bot.reply_to(message, "Привмяу, че кого?")
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEDZithqEibdkEMdfQwFqCihlP_XzwZmQACYQEAAixg9RwjJ4QiDIzt8CIE")
 
 bot.polling()
